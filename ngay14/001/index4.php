f69844b399f9427ce0581c51f7943cf06d8addfc
dat <dathalongbay@gmail.com>
2020-11-12 20:00:27 +0700

    111

--- ngay14/001/index4.php
+++ ngay14/001/index4.php
@@ -8,6 +8,33 @@
 <body>
     <h1>in ra tổng các số chẵn từ 1 đến 50 bằng php for</h1>
 
+    <?php 
+    $tong = 0;
+    for ($i = 1; $i <= 50; $i++) {
+        if ($i % 2 == 0) {
+            $tong += $i;
+        }
+    }
+
+    echo "tổng : " . $tong;
+    ?>
+
+    <script>
+
+    var tong = 0;
+
+    for (var i = 1; i <= 50; i++) {
+
+        if (i % 2 == 0) {
+            tong += i;
+            //tong = tong + i;
+        }
+    }
+
+    console.log(tong);
+
+    </script>
+
 
 </body>
 </html>
