08b1c02115b09b8663ff188fbecc8b6377f8051a
dat <dathalongbay@gmail.com>
2020-11-12 19:37:58 +0700

    1111

---
+++ ngay14/001/index3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $a = 5;
+    $b = 2;
+    echo "<br> tổng : " . ($a + $b);
+    echo "<br> hiệu : " . ($a - $b);
+    echo "<br> tích : " . ($a * $b);
+    echo "<br> thương : " . ($a/$b);
+    echo "<br> phép chia lấy dư : " . ($a % $b);
+
+    ?>
+</body>
+</html>
