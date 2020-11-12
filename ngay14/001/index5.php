8595ab604e5ffd9c3470de6bf7faf25f929599fa
dat <dathalongbay@gmail.com>
2020-11-12 19:43:21 +0700

    111

---
+++ ngay14/001/index5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>in ra tổng các số chẵn từ 1 đến 50 bằng php while</h1>
+
+
+</body>
+</html>
