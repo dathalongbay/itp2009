3c9066084d2a755674b602a5902f72d2c3f943b4
datdofpt <dathalongbay@gmail.com>
2020-11-24 20:34:30 +0700

    Add files via upload

---
+++ ngay17/004/home.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="">Trang chủ</a> </li>
+        <li><a href="">Giới thiệu</a> </li>
+        <li><a href="http://localhost/itp2009/ngay17/004/contact.php?controller=post&action=edit&id=12">Liên hệ</a> </li>
+    </ul>
+
+</body>
+</html>
