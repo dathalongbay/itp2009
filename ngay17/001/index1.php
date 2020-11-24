701989e5ad319a33e87f73dd1115918f253e2608
datdofpt <dathalongbay@gmail.com>
2020-11-24 19:50:08 +0700

    Update index1.php

--- ngay17/001/index1.php
+++ ngay17/001/index1.php
@@ -9,6 +9,35 @@
 </head>
 <body>
 
+    <?php
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+    // đặt mặc định cho các biến
+    $days = 1;
+    $type_room = 1;
+    $child = 0;
+    $vat = 0;
+
+    if (isset($_POST["days"]) && isset($_POST["type_room"])) {
+        $days = $_POST["days"];
+        $type_room = $_POST["type_room"];
+
+        // cú pháp viết rút gọn if else
+        /*if (isset($_POST["child"]) && ($_POST["child"] == 1)) {
+            $child = 1;
+        } else {
+            $child = 0;
+        }*/
+        // $bien = (điều kiện) ? nếu điều kiện true : nếu điều kiện false ;
+        $child = isset($_POST["child"]) && ($_POST["child"] == 1) ? 1 : 0;
+        $vat = isset($_POST["vat"]) && ($_POST["vat"] == 1) ? 1 : 0;
+
+
+    }
+    ?>
+
     <h1>Đặt phòng du lịch</h1>
 
     <pre>
@@ -26,25 +55,60 @@
     <form name="tour" action="" method="post">
         <p>
             <label for="">Tổng số ngày</label>
-            <input type="number" name="days" value="1">
+            <input type="number" name="days" value="<?php echo $days; ?>">
         </p>
         <p>
             <label for="">Loại phòng</label>
             <select name="type_room">
-                <option value="1">Phòng đơn</option>
-                <option value="2">Phòng đôi</option>
-                <option value="3">Phòng VIP</option>
+                <?php
+                if (isset($_POST["type_room"]) && ($_POST["type_room"] == 1)) {
+                    $selected1 = "selected";
+                } else {
+                    $selected1 = "";
+                }
+                ?>
+                <option value="1" <?php echo $selected1; ?>>Phòng đơn</option>
+                <?php
+                if (isset($_POST["type_room"]) && ($_POST["type_room"] == 2)) {
+                    $selected2 = "selected";
+                } else {
+                    $selected2 = "";
+                }
+                ?>
+                <option value="2" <?php echo $selected2; ?>>Phòng đôi</option>
+                <?php
+                if (isset($_POST["type_room"]) && ($_POST["type_room"] == 3)) {
+                    $selected3 = "selected";
+                } else {
+                    $selected3 = "";
+                }
+                ?>
+                <option value="3" <?php echo $selected3; ?>>Phòng VIP</option>
             </select>
         </p>
 
         <p>
             <label>Có trẻ em đi kèm</label>
-            <input type="checkbox" name="child" value="1">
+            <?php
+            if (isset($_POST["child"]) && ($_POST["child"] == 1)) {
+                $checked1 = "checked";
+            } else {
+                $checked1 = "";
+            }
+            ?>
+            <input type="checkbox" <?php echo $checked1 ?> name="child" value="1">
         </p>
 
         <p>
             <label>Xuất hóa đơn GTGT 10%</label>
-            <input type="checkbox" name="vat" value="1">
+            <?php
+            if (isset($_POST["vat"]) && ($_POST["vat"] == 1)) {
+                $checked2 = "checked";
+            } else {
+                $checked2 = "";
+            }
+            ?>
+            <input type="checkbox" <?php echo $checked2 ?> name="vat" value="1">
         </p>
 
         <p>
