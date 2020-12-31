f2aaa1e09831576c908fbee9ffd14fcb93c844fe
dat <dathalongbay@gmail.com>
2020-12-31 19:15:28 +0700

    666

---
+++ ngay27mvc/class/db.php
@@ -0,0 +1,48 @@
+<?php
+class Database {
+
+    public $serverName = "localhost";
+    public $userName = "root";
+    public $password = "";
+    public $dbName = "dienthoai";
+    public $connection;
+
+    public function __construct()
+    {
+        if (!$this->connection) {
+            $this->connect();
+        }
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+        if ($this->connection) {
+            $this->disconnect();
+        }
+    }
+
+    public function connect() {
+
+        try {
+
+            $this->connection = new PDO("mysql:host=$this->serverName;dbname=$this->dbName", $this->userName, $this->password);
+
+            // set the PDO error mode to exception
+
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+        } catch(PDOException $e) {
+
+            echo "Connection failed: " . $e->getMessage();
+            die;
+
+        }
+    }
+
+    public function disconnect() {
+        $this->connection = null;
+    }
+
+}
