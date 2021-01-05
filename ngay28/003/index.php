e491e98f91c43c739da9b5e9e1dc25f088f9a2df
dat <dathalongbay@gmail.com>
2021-01-05 19:45:18 +0700

    h

---
+++ ngay28/003/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <pre>
+        ajax là gì ?
+        ajax là 1 kỹ thuật load dữ liệu hay load 1 phần trang web
+        mà không cần phải refresh F5 lại trang
+    </pre>
+
+</body>
+</html>
